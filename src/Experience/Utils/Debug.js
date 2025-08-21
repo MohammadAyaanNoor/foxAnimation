c11fb05da20533fbdd1b1b573472d1313a123365
Mohammad Ayaan Noor <rjayaan722@gmail.com>
2025-08-21 22:05:44 +0530

    third commit

--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -3,7 +3,7 @@ import GUI from "lil-gui"
 
 export default class Debug{
     constructor(){
-        this.active = window.location.hash === '#debug'
+        this.active = window.location.hash === ''
         if(this.active){
             this.ui = new GUI()
         }
