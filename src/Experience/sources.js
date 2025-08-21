13a250f22fe9023829f1d2303177c865f612d193
Mohammad Ayaan Noor <rjayaan722@gmail.com>
2025-08-21 22:03:02 +0530

    second commit

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,33 +1,29 @@
 export default [
     {
-        name:'environmentMapTexture',
-        type:'cubeTexture',
+        name: 'environmentMapTexture',
+        type: 'cubeTexture',
         path: [
-            '../public/static/textures/environmentMap/px.jpg',
-            '../public/static/textures/environmentMap/nx.jpg',
-            '../public/static/textures/environmentMap/py.jpg',
-            '../public/static/textures/environmentMap/ny.jpg',
-            '../public/static/textures/environmentMap/pz.jpg',
-            '../public/static/textures/environmentMap/nz.jpg',
+            '/static/textures/environmentMap/px.jpg',
+            '/static/textures/environmentMap/nx.jpg',
+            '/static/textures/environmentMap/py.jpg',
+            '/static/textures/environmentMap/ny.jpg',
+            '/static/textures/environmentMap/pz.jpg',
+            '/static/textures/environmentMap/nz.jpg',
         ] 
     },
     {
-        name:'grassColorTexture',
-        type:'texture',
-        path: [
-            '../public/static/textures/dirt/color.jpg'
-        ] 
+        name: 'grassColorTexture',
+        type: 'texture',
+        path: '/static/textures/dirt/color.jpg'
     },
     {
-        name:'grassNormalTexture',
-        type:'texture',
-        path: [
-            '../public/static/textures/dirt/normal.jpg'
-        ] 
+        name: 'grassNormalTexture',
+        type: 'texture',
+        path: '/static/textures/dirt/normal.jpg'
     },
     {
-        name:'foxModel',
-        type:'gltfModel',
-        path:'../public/static/models/Fox/glTF/Fox.gltf'
+        name: 'foxModel',
+        type: 'gltfModel',
+        path: '/static/models/Fox/glTF/Fox.gltf'
     }
 ]
